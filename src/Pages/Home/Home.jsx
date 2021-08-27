176007277cc370ad6d3b737a9cabea7dacc0b8d0
Valentine-Efagene <efagenevalentine@gmail.com>
2021-08-27 15:40:09 +0100

    update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,49 +9,49 @@ const Home = () => {
         <NavBar />
       </div>
       <div className="container">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac cursus tortor. Proin nec ante eu diam egestas
-          aliquam ac sit amet quam. Praesent tincidunt erat diam. Nam ullamcorper magna erat, in tincidunt eros hendrerit
-          sed. Nulla nec risus elit. In dictum mollis porta. Nulla facilisi. Maecenas ut lectus urna. Aenean nec rhoncus
-          nibh, nec venenatis augue. Aliquam at metus aliquet, posuere nisl ac, maximus tellus.
-        </p>
         <div>
           <img src="https://images.unsplash.com/photo-1603739903239-8b6e64c3b185?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80s" alt="img" />
         </div>
-      </div>
-      <div className="container-reversed">
-        <div>
-          <img src="https://images.unsplash.com/photo-1603739903239-8b6e64c3b185?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80" alt="img" />
-        </div>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac cursus tortor. Proin nec ante eu diam egestas
           aliquam ac sit amet quam. Praesent tincidunt erat diam. Nam ullamcorper magna erat, in tincidunt eros hendrerit
           sed. Nulla nec risus elit. In dictum mollis porta. Nulla facilisi. Maecenas ut lectus urna. Aenean nec rhoncus
           nibh, nec venenatis augue. Aliquam at metus aliquet, posuere nisl ac, maximus tellus.
         </p>
+      </div>
+      <div className="container-reversed">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac cursus tortor. Proin nec ante eu diam egestas
+          aliquam ac sit amet quam. Praesent tincidunt erat diam. Nam ullamcorper magna erat, in tincidunt eros hendrerit
+          sed. Nulla nec risus elit. In dictum mollis porta. Nulla facilisi. Maecenas ut lectus urna. Aenean nec rhoncus
+          nibh, nec venenatis augue. Aliquam at metus aliquet, posuere nisl ac, maximus tellus.
+        </p>
+        <div>
+          <img src="https://images.unsplash.com/photo-1603739903239-8b6e64c3b185?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80" alt="img" />
+        </div>
       </div>
       <h1 className="title">Contacts</h1>
       <div className="container">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac cursus tortor. Proin nec ante eu diam egestas
-          aliquam ac sit amet quam. Praesent tincidunt erat diam. Nam ullamcorper magna erat, in tincidunt eros hendrerit
-          sed. Nulla nec risus elit. In dictum mollis porta. Nulla facilisi. Maecenas ut lectus urna. Aenean nec rhoncus
-          nibh, nec venenatis augue. Aliquam at metus aliquet, posuere nisl ac, maximus tellus.
-        </p>
         <div>
           <img className="avatar-img" src="https://www.pngarts.com/files/5/Cartoon-Avatar-Transparent-Image.png" alt="img" />
         </div>
-      </div>
-      <div className="container-reversed">
-        <div small-img>
-          <img className="avatar-img" src="https://www.pngkey.com/png/full/119-1195864_avatar-transparent-female-cartoon.png" alt="img" />
-        </div>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac cursus tortor. Proin nec ante eu diam egestas
           aliquam ac sit amet quam. Praesent tincidunt erat diam. Nam ullamcorper magna erat, in tincidunt eros hendrerit
           sed. Nulla nec risus elit. In dictum mollis porta. Nulla facilisi. Maecenas ut lectus urna. Aenean nec rhoncus
           nibh, nec venenatis augue. Aliquam at metus aliquet, posuere nisl ac, maximus tellus.
         </p>
+      </div>
+      <div className="container-reversed">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac cursus tortor. Proin nec ante eu diam egestas
+          aliquam ac sit amet quam. Praesent tincidunt erat diam. Nam ullamcorper magna erat, in tincidunt eros hendrerit
+          sed. Nulla nec risus elit. In dictum mollis porta. Nulla facilisi. Maecenas ut lectus urna. Aenean nec rhoncus
+          nibh, nec venenatis augue. Aliquam at metus aliquet, posuere nisl ac, maximus tellus.
+        </p>
+        <div small-img>
+          <img className="avatar-img" src="https://www.pngkey.com/png/full/119-1195864_avatar-transparent-female-cartoon.png" alt="img" />
+        </div>
       </div>
     </div>
   )
